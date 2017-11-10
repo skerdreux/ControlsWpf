4f2ee39473b7009176457fb29fa13a8c8bf73048
sylvain <sylvain.kerdreux@capgemini.com>
2017-11-10 17:18:56 +0100

    <FileExplorer> - Mise en place du projet
    Projet du userControl et du test
    Parsing des répertoires et des disques
    TODO : gérer TOUS les disques

---
+++ FileExplorer/FileExplorer/Converters/HeaderToImageConverter.cs
@@ -0,0 +1,91 @@
+﻿namespace FileExplorer.Converters
+{
+    #region Usings
+
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows.Data;
+    using System.Windows.Media.Imaging;
+    using global::FileExplorer.Properties;
+
+    #endregion
+
+    public class HeaderToImageConverter : IValueConverter
+    {
+        #region Champs et constantes statiques
+
+        public static readonly HeaderToImageConverter Instance =
+            new HeaderToImageConverter();
+
+        #endregion
+
+        #region Méthodes publiques
+
+        /// <summary>
+        ///     Convertit une valeur.
+        /// </summary>
+        /// <param name="value">Valeur produite par la source de liaison.</param>
+        /// <param name="targetType">Type de la propriété de cible de liaison.</param>
+        /// <param name="parameter">Paramètre de convertisseur à utiliser.</param>
+        /// <param name="culture">Culture à utiliser dans le convertisseur.</param>
+        /// <returns>
+        ///     Valeur convertie. Si la méthode retourne null, la valeur null valide est utilisée.
+        /// </returns>
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture)
+        {
+            if (value != null && ((string) value).Contains(@"\"))
+            {
+                return ToBitmapImage(ResourceImage.drive);
+            }
+            return ToBitmapImage(ResourceImage.folder);
+        }
+
+        /// <summary>
+        ///     Convertit une valeur.
+        /// </summary>
+        /// <param name="value">Valeur produite par la cible de liaison.</param>
+        /// <param name="targetType">Type vers lequel effectuer la conversion.</param>
+        /// <param name="parameter">Paramètre de convertisseur à utiliser.</param>
+        /// <param name="culture">Culture à utiliser dans le convertisseur.</param>
+        /// <returns>
+        ///     Valeur convertie. Si la méthode retourne null, la valeur null valide est utilisée.
+        /// </returns>
+        /// <exception cref="System.NotSupportedException">Cannot convert back</exception>
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture) =>
+            throw new NotSupportedException("Cannot convert back");
+
+        #endregion
+
+        #region Méthodes privées
+
+        /// <summary>
+        ///     To the bitmap image.
+        /// </summary>
+        /// <param name="bitmap">The bitmap.</param>
+        /// <returns></returns>
+        private static BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ FileExplorer/FileExplorer/FileExplorer.xaml.cs
@@ -0,0 +1,206 @@
+﻿namespace FileExplorer
+{
+    #region Usings
+
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    #endregion
+
+    /// <summary>
+    ///     Logique d'interaction pour FileExplorer.xaml
+    /// </summary>
+    public partial class FileExplorer
+    {
+        #region Champs et constantes statiques
+
+        public static readonly DependencyProperty PatternFileProperty =
+            DependencyProperty.Register("PatternFile", typeof(string),
+                                        typeof(FileExplorer), new PropertyMetadata(""));
+
+        /// <summary>
+        ///     The selected file property
+        /// </summary>
+        public static readonly DependencyProperty SelectedFileProperty =
+            DependencyProperty.Register("SelectedFile", typeof(string),
+                                        typeof(FileExplorer), new PropertyMetadata(""));
+
+        /// <summary>
+        ///     The selected file property
+        /// </summary>
+        public static readonly DependencyProperty DefaultDriveProperty =
+            DependencyProperty.Register("DefaultDrive", typeof(string),
+                                        typeof(FileExplorer), new PropertyMetadata(""));
+
+        #endregion
+
+        #region Champs
+
+        private readonly object dummyNode = null;
+
+        #endregion
+
+        #region Constructeurs et destructeurs
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="FileExplorer" /> class.
+        /// </summary>
+        public FileExplorer()
+        {
+            this.InitializeComponent();
+
+            // on sélectionne le disque par défaut si non saisie
+            if (string.IsNullOrEmpty(this.DefaultDrive))
+            {
+                this.DefaultDrive = Path.GetPathRoot(Assembly.GetExecutingAssembly().Location);
+            }
+
+            this.ListDirectory(this.TreeViewFolder, this.DefaultDrive);
+
+            this.TreeViewFolder.SelectedItemChanged += this.TreeViewFolderOnSelectedItemChanged;
+        }
+
+        #endregion
+
+        #region Propriétés et indexeurs
+
+        /// <summary>
+        ///     Gets or sets the default drive.
+        /// </summary>
+        /// <value>
+        ///     The default drive.
+        /// </value>
+        public string DefaultDrive
+        {
+            get => (string) this.GetValue(DefaultDriveProperty);
+            set => this.SetValue(DefaultDriveProperty, value);
+        }
+
+
+        /// <summary>
+        ///     Gets or sets the pattern file.
+        /// </summary>
+        /// <value>
+        ///     The pattern file.
+        /// </value>
+        public Regex PatternFile
+        {
+            get
+            {
+                var regexp = new Regex(this.GetValue(PatternFileProperty).ToString());
+                return regexp;
+            }
+            set => this.SetValue(PatternFileProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the selected file.
+        /// </summary>
+        /// <value>
+        ///     The selected file.
+        /// </value>
+        public string SelectedFile
+        {
+            get => (string) this.GetValue(SelectedFileProperty);
+            set => this.SetValue(SelectedFileProperty, value);
+        }
+
+        #endregion
+
+        #region Méthodes privées
+
+        /// <summary>
+        ///     Creates the directory node.
+        /// </summary>
+        /// <param name="directoryInfo">The directory information.</param>
+        /// <returns></returns>
+        private TreeViewItem CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeViewItem
+            {
+                Header = directoryInfo.Name,
+                Tag = directoryInfo.Name,
+                FontWeight = FontWeights.Normal,
+                Items = {this.dummyNode}
+            };
+            directoryNode.Expanded += this.FolderExpanded;
+
+            return directoryNode;
+        }
+
+        /// <summary>
+        /// Folders the expanded.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void FolderExpanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem) sender;
+            if (item.Items.Count == 1 && item.Items[0] == this.dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (var directoryPath in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        var subitem = new TreeViewItem
+                        {
+                            Header = directoryPath.Substring(
+                                directoryPath.LastIndexOf("\\", StringComparison.Ordinal) + 1),
+                            Tag = directoryPath,
+                            FontWeight = FontWeights.Normal
+                        };
+                        if (Directory.GetDirectories(directoryPath).Length > 0)
+                        {
+                            subitem.Items.Add(this.dummyNode);
+                        }
+                        subitem.Expanded += this.FolderExpanded;
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored ==> dans le cas d'un accès impossible à un répertoire
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Lists the directory.
+        /// </summary>
+        /// <param name="treeView">The tree view.</param>
+        /// <param name="path">The path.</param>
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Items.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Items.Add(this.CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        /// <summary>
+        ///     TreeViews the folder on selected item changed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="routedPropertyChangedEventArgs">The <see cref="object" /> instance containing the event data.</param>
+        private void TreeViewFolderOnSelectedItemChanged(object sender,
+                                                         RoutedPropertyChangedEventArgs<object>
+                                                             routedPropertyChangedEventArgs)
+        {
+            // récupération du répertoire
+            var fullPath = ((sender as TreeView)?.SelectedItem as TreeViewItem)?.Tag.ToString();
+            if (!string.IsNullOrEmpty(fullPath))
+            {
+                var listFiles = Directory.GetFiles(fullPath).Where(path => this.PatternFile.IsMatch(path))
+                                         .Select(path => new FileInfo(path).Name).ToList();
+                this.ListFichier.ItemsSource = listFiles;
+            }
+        }
+
+        #endregion
+    }
+}
